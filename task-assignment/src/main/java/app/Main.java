9bfefac97dac87e57b801a4a652f1ff113a090c3
Donato Marrazzo <donato.marrazzo@gmail.com>
2018-05-02 16:59:49 +0200

    fixed Skill to manage contains. Fancy text output.

--- task-assignment/src/main/java/app/Main.java
+++ task-assignment/src/main/java/app/Main.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
 
@@ -78,11 +79,29 @@ public class Main {
 	private void printSolution(TaskAssagnmentSolution solution) {
 		List<Employee> employees = solution.getEmployeeList();
 		
+		
 		for (Employee employee : employees) {
 			System.out.print(employee.getFullName() + " - ");
 			Task task = employee.getNextTask();
 			while (task != null) {
-				System.out.print(task.getTaskType().getId()+ ", ");
+				StringBuffer taskStr = new StringBuffer();
+				taskStr.append(task.getTaskType().getId());
+				switch (task.getPriority()) {
+				case URGENT:
+					taskStr.append("!");
+					break;
+
+				case NORMAL:
+					taskStr.append(":");
+					break;
+
+				default:
+					taskStr.append(".");
+					break;
+				}
+				taskStr.append(StringUtils.repeat("#", task.getTaskType().getDuration()));
+				taskStr.append(" ");
+				System.out.print(taskStr);
 				task = task.getNextTask();
 			}
 			System.out.println();
--- task-assignment/src/main/java/domain/Skill.java
+++ task-assignment/src/main/java/domain/Skill.java
@@ -2,15 +2,19 @@ package domain;
 
 import java.io.Serializable;
 
+import org.optaplanner.core.api.domain.lookup.PlanningId;
+
 public class Skill implements Serializable {
 
 	private static final long serialVersionUID = -6463704897223188538L;
+
 	private String name;
 
 	public Skill(String name) {
 		this.name = name;
 	}
 
+	@PlanningId
 	public String getName() {
 		return name;
 	}
@@ -23,4 +27,24 @@ public class Skill implements Serializable {
 	public String toString() {
 		return String.format("%s", name);
 	}
+	
+	/**
+	 * Tweak to make work {@link java.util.Set#contains(Object)}
+	 */
+	@Override
+	public int hashCode() {
+		return getName().hashCode();
+	}
+	
+	/**
+	 * Tweak to make work {@link java.util.Set#contains(Object)}
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Skill)
+			return getName().equals(((Skill) o).getName());
+		else
+			return super.equals(o);
+	}
+
 }
