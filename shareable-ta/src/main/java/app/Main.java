e05c0cb3352c779156827e438204e27c1c359883
Donato Marrazzo <donato.marrazzo@gmail.com>
2018-05-21 17:11:47 +0200

    Main solving app: added arguments handling

--- shareable-ta/src/main/java/app/Main.java
+++ shareable-ta/src/main/java/app/Main.java
@@ -11,26 +11,36 @@ import domain.Interval;
 import domain.TaskAssagnmentSolution;
 
 public class Main {
-	public void solve() {
+	public void solve(String employees, String tasks, boolean save) {
 		SolverFactory<TaskAssagnmentSolution> solverFactory = SolverFactory.createFromXmlResource("solver/taskAssignmentSolverConfig.xml");
 		Solver<TaskAssagnmentSolution> solver = solverFactory.buildSolver();
 
-		TaskAssagnmentSolution unsolved = ProblemBuilder.readProblemFacts("data/employees-3.txt", "data/tasks-8.txt");
+		TaskAssagnmentSolution unsolved = ProblemBuilder.readProblemFacts(String.format("data/employees-%s.txt", employees), String.format("data/tasks-%s.txt", tasks));
 
 		Set<Interval> gaps = new HashSet<>();
 		Interval brief = new Interval(LocalTime.parse("08:00"), LocalTime.parse("08:30"));
 		gaps.add(brief);
 		unsolved.setGaps(gaps);
 
+		ProblemBuilder.save(unsolved, String.format("data/emp-%s-task-%s.xml",employees,tasks));
+
+		
 		TaskAssagnmentSolution solved = solver.solve(unsolved);
 		ProblemBuilder.printSolution(solved);
-		
-		// ProblemBuilder.save(solved, "data/emp-3-task-8.xml");
+
+		ProblemBuilder.save(solved, String.format("data/emp-%s-task-%s-solved.xml",employees,tasks));
 	}
 
 	public static void main(String[] args) {
 		Main main = new Main();
+		boolean save = false;
+		if (args.length == 3)
+			save=args[2].equalsIgnoreCase("save");
+		
+		if (args.length >= 2)
+			main.solve(args[0], args[1], save);
+		else
+			main.solve("3", "8", save);
 
-		main.solve();
 	}
 }
